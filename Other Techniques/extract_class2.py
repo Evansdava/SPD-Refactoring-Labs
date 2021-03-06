79198731e7d1137fb238f2c9c5cd7a3421226d4a
David Evans <evansdav.a@gmail.com>
2021-03-05 16:35:08 -0800

    Added Other Techniques

--- Other Techniques/extract_class2.py
+++ Other Techniques/extract_class2.py
@@ -1,20 +1,39 @@
 # by Kami Bigdely
 # Extract class
-first_names = ['elizabeth', 'Jim']
-last_names = ['debicki', 'Carrey']
-birth_year = [1990, 1962]
-movies = [['Tenet', 'Vita & Virgina', 'Guardians of the Galexy', 'The Great Gatsby'],\
-     ['Ace Ventura', 'The Mask', 'Dubm and Dumber', 'The Truman Show', 'Yes Man']]
-emails = ['deb@makeschool.com', 'jim@makeschool.com']
+
+
+class Actor():
+    """Class for actors"""
+
+    def __init__(self, first_name, last_name, birth_year, movies, email):
+        """Initialize variables"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
+        self.movies = movies
+        self.email = email
+
+
+e_debicki = Actor('elizabeth', 'debicki', 1990,
+                  ['Tenet', 'Vita & Virgina', 'Guardians of the Galexy',
+                   'The Great Gatsby'], 'deb@makeschool.com')
+
+j_carrey = Actor('Jim', 'Carrey', 1962,
+                 ['Ace Ventura', 'The Mask', 'Dubm and Dumber',
+                  'The Truman Show', 'Yes Man'], 'jim@makeschool.com')
+
+actors = [e_debicki, j_carrey]
+
 
 def send_hiring_email(email):
     print("email sent to: ", email)
-    
-for i, value in enumerate(emails):
-    if birth_year[i] > 1985:
-        print(first_names[i], last_names[i])
+
+
+for actor in actors:
+    if actor.birth_year > 1985:
+        print(actor.first_name, actor.last_name)
         print('Movies Played: ', end='')
-        for m in movies[i]:
+        for m in actor.movies:
             print(m, end=', ')
         print()
-        send_hiring_email(value)
+        send_hiring_email(actor.email)
--- Other Techniques/extract_class3.py
+++ Other Techniques/extract_class3.py
@@ -5,31 +5,43 @@ MEDIUM = 2500
 COOKED_CONSTANT = 0.05
 
 
-def is_cookeding_criteria_satisfied(time, temperature, 
-                                    pressure, desired_state):
-    return is_well_done(time, temperature, pressure, desired_state) or \
-           is_medium(time, temperature, pressure, desired_state)
+class PreparedFood():
+    """Class for determining food cooked state"""
+
+    def __init__(self, time, temp, pressure, desired_state):
+        """Initialize variables"""
+        self.time = time
+        self.temp = temp
+        self.pressure = pressure
+        self.desired_state = desired_state
+
+    def is_cookeding_criteria_satisfied(self):
+        """Check whether cooked to desired state"""
+        return self.is_well_done() or self.is_medium()
+
+    def is_well_done(self):
+        """Check if cooked well done"""
+        return (self.desired_state == 'well-done'
+                and self.get_cooking_progress() >= WELL_DONE)
+
+    def is_medium(self):
+        """Check if cooked medium"""
+        return (self.desired_state == 'medium'
+                and self.get_cooking_progress() >= MEDIUM)
+
+    def get_cooking_progress(self):
+        """Return how cooked food is"""
+        return self.time * self.temperature * self.pressure * COOKED_CONSTANT
 
 
-def is_well_done(time, temperature, pressure, desired_state):    
-    return desired_state == 'well-done' and  \
-           get_cooking_progress(time, temperature, pressure) >= WELL_DONE
-
-
-def is_medium(time, temperature, pressure, desired_state):
-    return desired_state == 'medium' and  \
-           get_cooking_progress(time, temperature, pressure) >= MEDIUM
-
-def get_cooking_progress(time, temperature, pressure):
-    return time * temperature * pressure * COOKED_CONSTANT
-
-
-time = 30 # [min]
-temp = 103 # [celcius]
-pressure = 20 # [psi]
+time = 30  # [min]
+temp = 103  # [celcius]
+pressure = 20  # [psi]
 desired_state = 'well-done'
 
-if is_cookeding_criteria_satisfied(time, temp, pressure, desired_state):
+food = PreparedFood(time, temp, pressure, desired_state)
+
+if food.is_cookeding_criteria_satisfied():
     print('cooking is done.')
 else:
     print('ongoing cooking.')
